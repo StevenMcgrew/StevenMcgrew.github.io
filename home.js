65d1153449b385cb7e12f8b601c451937937975c
StevenMcgrew <stevenmcgrew8224@msn.com>
2019-09-25 17:01:54 -0700

    abstractions

--- home.js
+++ home.js
@@ -1,25 +1,82 @@
 'use strict';
-// IIFE for Dropdown
+
 (function () {
 
-    let itemsPanel = document.getElementById('itemsPanel')
-    let backdrop = document.getElementById('backdrop')
+    /***********************************************************
+    
+    Abstractions
+    
+    ************************************************************/
 
-    document.getElementById('dropdown').addEventListener('click', function () {
+    function element(id) { return document.getElementById(id) }
+    function click(element, callback) { element.addEventListener('click', function (e) { callback(e) }) }
+
+    /***********************************************************
+    
+    General variables
+    
+    ************************************************************/
+
+    let backdrop = element('backdrop')
+    let newVehPanel = element('newVehPanel')
+
+    /***********************************************************
+    
+    Top bar buttons
+    
+    ************************************************************/
+
+    click(element('menuBtn'), function () {
+        // open menu panel
+    })
+
+    click(element('h1HomeBtn'), function () {
+        // navigate to home page
+    })
+
+    click(element('accountBtn'), function () {
+        // open account options panel
+    })
+
+    /***********************************************************
+    
+    Dropdown
+    
+    ************************************************************/
+
+    let itemsPanel = element('itemsPanel')
+
+    click(element('dropdown'), function () {
         itemsPanel.style.display = 'block'
         backdrop.style.display = 'block'
     })
 
-    backdrop.addEventListener('click', function () {
+    click(backdrop, function () {
+        itemsPanel.style.display = 'none'
+        newVehPanel.style.display = 'none'
+        backdrop.style.display = 'none'
+    })
+
+    click(itemsPanel, function (e) {
+        e.stopPropagation()
+        let vehicle = e.target.dataset.vehicle
+        if (vehicle) {
+            element('vin').textContent = e.target.id
+            element('vehicle').textContent = vehicle
+        }
         itemsPanel.style.display = 'none'
         backdrop.style.display = 'none'
     })
 
-    itemsPanel.addEventListener('click', function (e) {
-        e.stopPropagation()
-        document.getElementById('selectedItem').textContent = e.target.textContent
-        itemsPanel.style.display = 'none'
-        backdrop.style.display = 'none'
+    /***********************************************************
+    
+    New Vehicle
+    
+    ************************************************************/
+
+    click(element('newVehicleBtn'), function () {
+        newVehPanel.style.display = 'block';
+        backdrop.style.display = 'block'
     })
 
 })()
